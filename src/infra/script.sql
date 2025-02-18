13910ee1955df31cb221da75d2e5ed80f430123e
esaLCuE <louischenot@laposte.net>
2025-02-18 16:27:09 +0100

    Update script.sql

--- src/infra/script.sql
+++ src/infra/script.sql
@@ -15,8 +15,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE springboot;
-use springboot;
+CREATE DATABASE `springboot`;
+USE `springboot`;
 
 --
 -- Table structure for table `person`
